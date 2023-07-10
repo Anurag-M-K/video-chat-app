f679b7d69c149ee4068e4fe303d15abc3d5c820d
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2023-07-10 13:29:11 +0530

    first commit

---
+++ src/pages/room/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+function index() {
+  const { roomId } = useParams();
+
+  const myMeeting = async (element) => {
+    const appID = 2116028587;
+    const serverSecret = "a488c05a73239efb4643a2124537711c";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "Anurag MK"
+    );
+    const zc = ZegoUIKitPrebuilt.create(kitToken)
+    zc.joinRoom({
+      container: element,
+      sharedLinks :[
+        {
+          name: "Copy Link",
+          url :`http://localhost:5173/room/${roomId}`
+        }
+      ],
+      scenario :{
+        mode:ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton : false,
+    })
+  };
+  return <div>
+    <div ref={myMeeting} />
+  </div>;
+}
+
+export default index;
